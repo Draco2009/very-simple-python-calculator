dd7548f37d22510f2604bedb550a0eeb89a9196b
Draco2009 <92245477+Draco2009@users.noreply.github.com>
2021-11-11 09:46:05 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,26 +5,35 @@ print('2. subtraction')
 print('3. multiplication')
 print('4. division')
 print('give 1 as input for addition, 2 for subtraction and vise versa')
+
 # input
+
 operator = input()
+
+# if
 if operator == '1':
     num1 = input('enter the first number: ')
     num2 = input('enter the second number: ')
     print(float(num1) + float(num2))
 
+    
 elif operator == '2':
     num1 = input('enter the first number: ')
     num2 = input('enter the second number: ')
     print(float(num1) - float(num2))
-    
+ 
+
 elif operator == '3':
     num1 = input('enter the first number: ')
     num2 = input('enter the second number: ')
     print(float(num1) * float(num2))
-    
+ 
+
 elif operator == '4':
     num1 = input('enter the first number: ')
     num2 = input('enter the second number: ')
     print(float(num1) / float(num2))
-    
+ 
+
+# done
 print('mission completed successfully *dies*')
